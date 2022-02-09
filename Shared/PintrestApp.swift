a71941d73c878ff947f80fae35c370995556e96a
TK <synova_matare@hotmail.co.uk>
2022-02-09 12:37:52 +0000

    Initial Commit

---
+++ Shared/PintrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PintrestApp.swift
+//  Shared
+//
+//  Created by Carlos on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PintrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
